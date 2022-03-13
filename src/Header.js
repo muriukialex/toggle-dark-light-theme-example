706db2fbcbc6f1b1ca84935e3f6457a3c30dabbc
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-03-14 00:16:39 +0300

    add Header component

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import { useContext } from 'react'
+import { ThemeContext } from './ThemeContext/ThemeContext'
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { HiOutlineSun } from 'react-icons/hi'
+
+const Header = () => {
+	const { theme, changeTheme } = useContext(ThemeContext)
+
+	return (
+		<>
+			<h1>Enable {theme.theme === 'dark' ? 'light' : 'dark'} mode</h1>
+			<span
+				onClick={() => changeTheme(theme.isLight)}
+				style={{ cursor: 'pointer' }}
+			>
+				{theme.theme === 'dark' ? (
+					<HiOutlineSun />
+				) : (
+					<BsFillMoonStarsFill />
+				)}
+			</span>
+		</>
+	)
+}
+
+export default Header
