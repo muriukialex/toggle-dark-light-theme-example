fbdbcf18c949ab4338ca54ff9425b98aabe26c74
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-03-14 00:18:57 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import { ThemeContextProvider } from './ThemeContext/ThemeContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [theme, setTheme] = useState({
+		theme: 'light',
+		isLight: true,
+	})
+
+	const changeTheme = (option) => {
+		//option is boolean
+		if (option) {
+			document.body.classList.add('dark-background')
+			setTheme({
+				theme: 'dark',
+				isLight: false,
+			})
+		} else {
+			document.body.classList.remove('dark-background')
+			setTheme({
+				theme: 'light',
+				isLight: true,
+			})
+		}
+	}
+
+	return (
+		<ThemeContextProvider
+			value={{
+				theme: theme,
+				changeTheme: changeTheme,
+			}}
+		>
+			<div className="App">
+				<Header />
+			</div>
+		</ThemeContextProvider>
+	)
 }
-
-export default App;
