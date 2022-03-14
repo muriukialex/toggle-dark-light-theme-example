653b1ff3f345ba894fae2e17562c787da6379c66
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-03-14 18:26:32 +0300

    reformat using prettier

--- src/App.js
+++ src/App.js
@@ -4,38 +4,38 @@ import Header from './Header'
 import { ThemeContextProvider } from './ThemeContext/ThemeContext'
 
 export default function App() {
-	const [theme, setTheme] = useState({
-		theme: 'light',
-		isLight: true,
-	})
+  const [theme, setTheme] = useState({
+    theme: 'light',
+    isLight: true,
+  })
 
-	const changeTheme = (option) => {
-		//option is boolean
-		if (option) {
-			document.body.classList.add('dark-background')
-			setTheme({
-				theme: 'dark',
-				isLight: false,
-			})
-		} else {
-			document.body.classList.remove('dark-background')
-			setTheme({
-				theme: 'light',
-				isLight: true,
-			})
-		}
-	}
+  const changeTheme = (option) => {
+    //option is boolean
+    if (option) {
+      document.body.classList.add('dark-background')
+      setTheme({
+        theme: 'dark',
+        isLight: false,
+      })
+    } else {
+      document.body.classList.remove('dark-background')
+      setTheme({
+        theme: 'light',
+        isLight: true,
+      })
+    }
+  }
 
-	return (
-		<ThemeContextProvider
-			value={{
-				theme: theme,
-				changeTheme: changeTheme,
-			}}
-		>
-			<div className="App">
-				<Header />
-			</div>
-		</ThemeContextProvider>
-	)
+  return (
+    <ThemeContextProvider
+      value={{
+        theme: theme,
+        changeTheme: changeTheme,
+      }}
+    >
+      <div className="App">
+        <Header />
+      </div>
+    </ThemeContextProvider>
+  )
 }
--- src/Header.js
+++ src/Header.js
@@ -4,19 +4,19 @@ import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { HiOutlineSun } from 'react-icons/hi'
 
 const Header = () => {
-	const { theme, changeTheme } = useContext(ThemeContext)
+  const { theme, changeTheme } = useContext(ThemeContext)
 
-	return (
-		<>
-			<h1>Enable {theme.theme === 'dark' ? 'light' : 'dark'} mode</h1>
-			<span
-				onClick={() => changeTheme(theme.isLight)}
-				style={{ cursor: 'pointer' }}
-			>
-				{theme.theme === 'dark' ? <HiOutlineSun /> : <BsFillMoonStarsFill />}
-			</span>
-		</>
-	)
+  return (
+    <>
+      <h1>Enable {theme.theme === 'dark' ? 'light' : 'dark'} mode</h1>
+      <span
+        onClick={() => changeTheme(theme.isLight)}
+        style={{ cursor: 'pointer' }}
+      >
+        {theme.theme === 'dark' ? <HiOutlineSun /> : <BsFillMoonStarsFill />}
+      </span>
+    </>
+  )
 }
 
 export default Header
